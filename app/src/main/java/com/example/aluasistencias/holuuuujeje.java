321d586d1d2b6a139aab0767a9223597a6d4bea5
Ikarosad-Shiro <desconexionparcial@gmail.com>
2025-01-20 03:35:28 -0600

    Resolucion de conflictos con los archivos DataHelper

--- app/src/main/java/com/example/aluasistencias/holuuuujeje.java
+++ app/src/main/java/com/example/aluasistencias/holuuuujeje.java
@@ -1,8 +1,5 @@
 package com.example.aluasistencias;
 
-<<<<<<<< HEAD:app/src/main/java/com/example/aluasistencias/holuuuujeje.java
-public class holuuuujeje {
-========
-public class HistorialRepartos {
->>>>>>>> origin/Michel:app/src/main/java/com/example/aluasistencias/HistorialRepartos.java
-}
+//public class holuuuujeje {
+
+//}
