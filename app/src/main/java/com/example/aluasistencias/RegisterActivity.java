aebb11d788b82acf27bf5ab360c0aa8e399fc718
Michel_becerril <angela_ab@tesch.edu.mx>
2025-01-20 22:58:14 -0600

    correcion de git

--- app/src/main/java/com/example/aluasistencias/RegisterActivity.java
+++ app/src/main/java/com/example/aluasistencias/RegisterActivity.java
@@ -1,6 +1,6 @@
 package com.example.aluasistencias;
 
-import android.content.Intent;
+import  android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
